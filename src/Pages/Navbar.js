6da08ca3a9605b0e08bbebd5490e87176c21d220
EnrisKumi <107574553+EnrisKumi@users.noreply.github.com>
2022-08-17 21:05:38 +0200

    .

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,14 +1,23 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Navbar(){
-    return(
-        <div className="w-full h-16 bg-blue-600 flex items-center px-10 py-2 justify-between">
-        
-        <Link to={'/'} className="text-white text-3xl font-semibold font-Montserrat">React App</Link>
-        <Link to="add-user" className="w-48 bg-white flex text-blue-400 justify-center items-center font-semibold tex-xl h-12 rounded-lg">Add User</Link>
-        </div>
-    )
+function Navbar() {
+  return (
+    <div className="w-full h-16 bg-blue-600 flex items-center px-10 py-2 justify-between">
+      <Link
+        to={"/"}
+        className="text-white text-3xl font-semibold font-Montserrat"
+      >
+        React App
+      </Link>
+      <Link
+        to="add-user"
+        className="w-48 bg-white flex text-blue-400 justify-center items-center font-semibold tex-xl h-12 rounded-lg"
+      >
+        Add User
+      </Link>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
